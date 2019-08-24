015d8f3ea1ad4e2f9df813ba2b086ab5c711ca40
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-24 04:07:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,12 +46,12 @@ def sum_array(array)
   sum
 end
 def add_s(array)
-  array.collect do |word|
-    if array [1] == word 
-      word 
-    else 
-      word + "s"
-    end 
-  end 
+   array.collect do |word|
+     if array [1] == word 
+       word 
+     else 
+       word + "s"
+     end 
+   end 
 end 
 
