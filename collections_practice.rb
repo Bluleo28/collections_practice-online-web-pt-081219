0458ee2104b13a81b97bacdda31163e731b422c1
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-24 04:17:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,12 +1,5 @@
 def sort_array_asc(array)
     array.sort
-    # array.sort do |a, b|
-    # if a < b 
-    #   -1
-    # elsif a > b 
-    #   1  
-    # else a == b
-    #   0 
   end
 
 def sort_array_desc(array)
