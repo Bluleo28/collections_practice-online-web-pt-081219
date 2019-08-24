575fc0b793d6d165841baedc1c3a3de547ae707d
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-24 04:23:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,12 +34,11 @@ end
 def sum_array(array)
   sum = 0 
   array.each do |num|
-    sum+=sum 
+    sum+=num 
   end
-  sum + x
+  sum
 end
 end
-sum_array([11,4,7,8,9,100,134]) #273
 
 def add_s(array)
         array.collect do |word|
